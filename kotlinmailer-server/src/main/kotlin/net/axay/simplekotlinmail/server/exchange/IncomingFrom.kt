9b50db32b0085a92475cc7cc7aeca63365b14027
David Bieregger <david@bieregger.at>
2022-06-08 08:42:57 +0200

    Remove server module and it's dependencies

--- kotlinmailer-server/src/main/kotlin/net/axay/simplekotlinmail/server/exchange/IncomingFrom.kt
+++
@@ -1,8 +0,0 @@
-package net.axay.simplekotlinmail.server.exchange
-
-import org.subethamail.smtp.MessageContext
-
-class IncomingFrom internal constructor(
-    val envelopeFrom: String,
-    context: MessageContext
-) : IncomingMailExchange(context)
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,7 +4,6 @@ rootProject.name = projectName
 
 include("$projectName-core")
 
-include("$projectName-server")
 include("$projectName-client")
 include("$projectName-html")
 
