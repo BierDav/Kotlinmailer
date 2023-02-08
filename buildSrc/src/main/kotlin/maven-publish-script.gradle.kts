c7755338ca9c99f4175c24e709a070369777340c
David Bieregger <david@bieregger.at>
2023-02-08 18:22:09 +0100

    Update publish plugin so that, it doesn't throw an error when no credentials are available. This made local development impossible

--- buildSrc/src/main/kotlin/maven-publish-script.gradle.kts
+++ buildSrc/src/main/kotlin/maven-publish-script.gradle.kts
@@ -14,8 +14,10 @@ signing {
     val privateKey = System.getenv("GPG_PRIVATE_KEY")
     val privateKeyPassword = System.getenv("GPG_PRIVATE_KEY_PASSWORD")
 
-    if (privateKey == null || privateKeyPassword == null)
-        throw Error("Private key and/or associated Password missing")
+    if (privateKey == null || privateKeyPassword == null) {
+        println("Private key and/or associated Password for signing is missing. This is not a problem for local development.")
+        return@signing
+    }
     useInMemoryPgpKeys(privateKey, privateKeyPassword)
 
     sign(publishing.publications)
